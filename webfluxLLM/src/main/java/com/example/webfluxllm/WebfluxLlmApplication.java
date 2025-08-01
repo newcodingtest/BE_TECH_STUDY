52b4bcae73f19e0339eb9a9b9c80ad11bd4d9a11
jyyoon <pulpul8282@naver.com>
2025-08-01 14:56:25 +0900

    ADD: LLM 템플릿

---
+++ webfluxLLM/src/main/java/com/example/webfluxllm/WebfluxLlmApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxllm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxLlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxLlmApplication.class, args);
+    }
+
+}
---
+++ webfluxLLM/src/main/java/com/example/webfluxllm/controller/ExampleController.java
@@ -0,0 +1,2 @@
+package com.example.webfluxllm.controller;public class ExampleController {
+}
